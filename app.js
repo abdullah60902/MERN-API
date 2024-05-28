31e3f011464130a319dc82250174f6582bc044fe
abdullah60902 <butt60902@gmail.com>
2024-05-28 10:27:13 +0500

    hello

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const Student = require('./api/route/students')
-const app = express();
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const User = require('./api/route/users')
-const Img = require('./api/route/img')
-const fileUpload = require('express-fileupload')
-const cors = require('cors')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
 const serverless = require('serverless-http');
+
+const app = express();
 
 mongoose.connect('mongodb+srv://abdullah:12345ab@abdullah.igvz11s.mongodb.net/?retryWrites=true&w=majority&appName=abdullah');
 
@@ -18,20 +16,23 @@ mongoose.connection.on('connected', () => {
     console.log('connection connected successful');
 });
 
-app.use(fileUpload({useTempFiles:true}))
+app.use(fileUpload({ useTempFiles: true }));
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/user',User)
-app.use("/student",Student)
-app.use('/img',Img)
+const Student = require('./api/route/students');
+const User = require('./api/route/users');
+const Img = require('./api/route/img');
 
-
+app.use('/user', User);
+app.use("/student", Student);
+app.use('/img', Img);
 
 app.use((req, res, next) => {
-    res.status(401).json({
-        meg:"not found"
-    })
+    res.status(404).json({
+        msg: "not found"
+    });
 });
-module.exports.handler =serverless( app);
+
+module.exports.handler = serverless(app);
