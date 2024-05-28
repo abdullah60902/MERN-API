a793539c0d983b0621499ebb2fceb332f6fb4432
abdullah60902 <butt60902@gmail.com>
2024-05-28 06:48:06 +0500

    hello

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
  const http = require('http')
  const app =require('./app')
  const server =http.createServer(app);
+ const serverless = require('serverless-http');
+
  server.listen(3000,()=>{
     console.log('run in 3000');
  })
+ module.exports = serverless(app);
