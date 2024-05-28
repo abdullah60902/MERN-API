d4ee4bf739a7b8b5559706f4d987b55cdc9ff2c5
abdullah60902 <butt60902@gmail.com>
2024-05-28 21:29:57 +0500

    hello

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
-
+const dotenv =require('dotenv')
+dotenv.config();
 
 const app = express();
 
